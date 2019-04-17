9cef9775c212ff4c61198a1c0559c66a87e1bcb4
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2019-04-18 01:50:59 +1000

    Initial commit + file systems tests

---
+++ GPG220_Activities_WALSH-JONES_T/Assets/File Systems/Scripts/JSONPreferences.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using UnityEngine.UI;
+
+public class JSONPreferences : MonoBehaviour
+{
+    public PlayerPrefsJSON JsonPrefs;
+
+    public Button saveJSON;
+    public Button loadJSON;
+
+    void Start()
+    {
+        saveJSON.onClick.AddListener(SaveToFile);
+        loadJSON.onClick.AddListener(LoadFromFile);
+    }
+
+    void SaveToFile()
+    {
+       string json = JsonUtility.ToJson(JsonPrefs, true);
+
+       Debug.Log("SAVED" + json);
+
+       File.WriteAllText(Path.Combine(Application.persistentDataPath, "playerProfile.json"), json);
+    }
+
+    void LoadFromFile()
+    {
+        string dataAsJson = "";
+        
+        dataAsJson = File.ReadAllText(Path.Combine(Application.persistentDataPath, "playerProfile.json"));
+        
+        JsonPrefs = JsonUtility.FromJson<PlayerPrefsJSON>(dataAsJson);
+        
+        Debug.Log("LOADED" + dataAsJson);
+    }
+}
---
+++ GPG220_Activities_WALSH-JONES_T/Assets/File Systems/Scripts/PlayerPreferences.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPreferences : MonoBehaviour
+{
+    public string playerName;
+
+    public Button savePreferences;
+    public Button loadPreferences;
+
+    void Start()
+    {
+        savePreferences.onClick.AddListener(SavePreferences);
+        loadPreferences.onClick.AddListener(LoadPreferences);
+    }
+
+    void SavePreferences()
+    {
+        PlayerPrefs.SetString("PlayerName", playerName);
+    }
+
+    void LoadPreferences()
+    {
+        if (PlayerPrefs.HasKey("PlayerName"))
+        {
+            playerName = PlayerPrefs.GetString("PlayerName");
+        }
+    }
+}
