257b20353d4117f1276f82d4d6e78839006f3a84
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2019-04-19 04:38:30 +1000

    More tests on the renderer, starting to think about stars

---
+++ GPG220_Activities_WALSH-JONES_T/Assets/Software Renderer/Star.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star
+{
+    public Vector3 position;
+    public int xPos = 0;
+    public int yPos = 0;
+    
+    void Start()
+    {
+        position = new Vector3(0,0,0);
+    }
+
+    void Update()
+    {
+        /*
+        position.x = 1;
+        position.y = 10;
+        */
+    }
+}
